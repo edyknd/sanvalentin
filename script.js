cb11d35f6ebb130009af04ef6fbc445600518f25
Edy <48465657+edyknd@users.noreply.github.com>
2026-02-14 10:11:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -141,7 +141,7 @@
     var textAnimate = eval(Jscex.compile("async", function () {
         var together = new Date();
         together.setFullYear(2025, 5, 24);
-        together.setHours(0);
+        together.setHours(10);
         together.setMinutes(0);
         together.setSeconds(0);
         together.setMilliseconds(0);
